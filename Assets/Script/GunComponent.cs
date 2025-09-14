38d893fd187782b676e015196415237b6a7fdf50
tarek-rf <tareqrf@gmail.com>
2025-09-14 11:22:21 -0400

    Complete Lab3

--- Assets/Script/GunComponent.cs
+++ Assets/Script/GunComponent.cs
@@ -9,5 +9,36 @@ public class GunComponent : MonoBehaviour
     private float chargeTime = 0.0f;
     private bool isCharging = false;
 
+    void Update()
+    {
+        // TODO add the logic to track player keeping the input down.
+        if (Input.GetButtonDown("Fire1"))
+        {
+            chargeTime = 0.0f;
+            isCharging = true;
+        }
+        if (Input.GetButton("Fire1"))
+        {
+            chargeTime += Time.deltaTime;
+            chargeTime = Mathf.Clamp(chargeTime, 0, maxChargeTime);
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            ShootBullet();
+            isCharging = false;
+        }
+    }
+
    
+    void ShootBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+       
+        // Scale bullet force based on charge time
+        float chargeRatio = chargeTime / maxChargeTime;
+        float bulletImpulse = Mathf.Pow(chargeRatio, 2) * bulletMaxImpulse * 2f;
+        rb.AddForce(bulletSpawnPoint.forward * bulletImpulse, ForceMode.Impulse);
+    }
+
 }
